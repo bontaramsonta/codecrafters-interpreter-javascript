b2f8ffd156cd6213f9b237a63bccd30324941d41
bontaramsonta <18cs113@gmail.com>
2025-03-18 16:37:05 +0530

    fix

--- app/main.js
+++ app/main.js
@@ -28,6 +28,7 @@ const SINGLE_CHAR_TOKENS = {
   "+": "PLUS + null",
   "-": "MINUS - null",
   ";": "SEMICOLON ; null",
+  "=": "EQUAL = null",
   "!": "BANG ! null",
   ">": "GREATER > null",
   "<": "LESS < null",
